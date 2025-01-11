9d74ba23c3b646f62dd55fe7d6a88aeea8b5484a
Upanisha <dupanisha@gmail.com>
2025-01-11 19:40:26 +0530

    index

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 3000;
+const mongoDBUrl = "mongodb+srv://dupanisha:fLGGL5Mb37ZqtxuO@cluster0.vbfvk.mongodb.net/";
+
+mongoose.connect(mongoDBUrl).then(() => {
+    console.log("Connected to MongoDB");
+    try{
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
+    }catch(err) {
+        console.log("Error starting server", err);
+    }
+}).catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+})
